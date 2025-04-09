579d3e0557a3b541c93f69bb89e7cf86709fafbb
ialexanderbrito <xanderzinho26@gmail.com>
2025-04-09 17:08:40 -0300

    fix: adjust table styles for better responsiveness and consistency

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b transition-colors data-[state=selected]:bg-[#1f1f1f]",
       className
     )}
     {...props}
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-9 px-4 align-middle font-normal text-muted-foreground [&:has([role=checkbox])]:pr-0 transition-colors",
       className
     )}
     {...props}
@@ -87,7 +87,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("px-4 py-2 align-middle [&:has([role=checkbox])]:pr-0 transition-colors", className)}
     {...props}
   />
 ))
